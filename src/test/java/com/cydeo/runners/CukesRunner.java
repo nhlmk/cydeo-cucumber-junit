d6e1ae5f01ffc1dccf581b4b506b7af4b699bd0c
nhlmk <nihalimek@gmail.com>
2022-06-27 17:04:04 +0300

    Some steps done

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,8 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report.html",
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
+                "junit:target/junit/junit-report.xml",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
